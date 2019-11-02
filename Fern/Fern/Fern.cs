d74d28c22c60621828018be859986ea10cdcf2e5
Jacob Bennett <jacobbennettn@gmail.com>
2019-11-01 21:53:31 -0400

    over

--- Fern/Fern/Fern.cs
+++ Fern/Fern/Fern.cs
@@ -20,12 +20,19 @@ namespace FernNamespace
          * Size: number of 3-pixel segments of tendrils
          * Redux: how much smaller children clusters are compared to parents
          * Turnbias: how likely to turn right vs. left (0=always left, 0.5 = 50/50, 1.0 = always right)
-         * canvas: the canvas that the fern will be drawn on
+         * canvas: the canvas that the fern will be drawn on  
          */
         public Fern(double directionFallOff, double lengthFallOff, double turnBias, Graphics graphics, int width, int height)
         {
-           Rectangle r = new Rectangle(0, 0, 200, 100);
-           graphics.DrawRectangle(new Pen(Color.Red, 2), r); 
+            Rectangle r = new Rectangle(0, 0, 200, 100);
+            graphics.DrawRectangle(new Pen(Color.Red, 2), r); 
+            branch(0, 0, 100, 100, graphics);
+        }
+
+        private void branch(int x1, int y1, int x2, int y2, Graphics graphics) {
+            Pen penny = (new Pen(Color.Green, 2));
+            graphics.DrawLine(penny, x1, y1, x2, y2);
+            //graphics.DrawCurve(penny, )
         }
     }
 }
