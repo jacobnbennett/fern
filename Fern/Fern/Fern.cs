5daa2feba8c8a109093d72331ed4de91ec86e315
Jacob Bennett <jacobbennettn@gmail.com>
2019-11-02 00:23:45 -0400

    over

--- Fern/Fern/Fern.cs
+++ Fern/Fern/Fern.cs
@@ -20,25 +20,24 @@ namespace FernNamespace
          * Size: number of 3-pixel segments of tendrils
          * Redux: how much smaller children clusters are compared to parents
          * Turnbias: how likely to turn right vs. left (0=always left, 0.5 = 50/50, 1.0 = always right)
-         * canvas: the canvas that the fern will be drawn on   rjwhreiu  e dsge 
+         * canvas: the canvas that the fern will be drawn on   rjwhreiu  e dsge    
          */
         public Fern(double size, double redux, double turnBias, Graphics graphics, int width, int height)
         {
             Rectangle r = new Rectangle(0, 0, 200, 100);
-            graphics.DrawRectangle(new Pen(Color.Red, 2), r); 
             
             Random rnd = new Random();
             double angle = Convert.ToDouble(90 * Math.PI / 180);
-            double length = Math.Sqrt(Math.Pow((300 - 300), 2) + (Math.Pow((200 - 600), 2)));
-            branch(300, 600, 300, 200, length * size, redux, angle, graphics);
+            double length = Math.Sqrt(Math.Pow((300 - 300), 2) + (Math.Pow((200 - 500), 2)));
+            branch(300, 500, 300, 200, length * size / 1.5, redux , angle, turnBias, graphics);
         }
 
-        private void branch(int x1, int y1, int x2, int y2, double length, double redux, double theta, Graphics graphics) {
+        private void branch(int x1, int y1, int x2, int y2, double length, double redux, double theta, double turnBias, Graphics graphics) {
             Pen penny = (new Pen(Color.Green, 2));
             //int x = x2;
             //int y = y2;
             
-            double nlength = length / 2;
+            double nlength = length / 1.55;
             int nx = Convert.ToInt32(x1 + length * Math.Cos(theta));
             int ny = Convert.ToInt32(y1 - length * Math.Sin(theta));
 
@@ -47,15 +46,20 @@ namespace FernNamespace
             
             graphics.DrawLine(penny, x1, y1, nx, ny);
            
-            if (redux > 0.1){
+            if (redux > 0){
                 
                 int nx2 = Convert.ToInt32(x2 + nlength * Math.Cos(theta));
                 int ny2 = Convert.ToInt32(y2 - nlength * Math.Sin(theta));
-                branch(nx, ny, nx2, ny2, nlength, redux - 0.1, theta + 10, graphics);
-                //branch(x2, y2, nx, ny, nlength, redux - 0.1, theta - 10, graphics);
+                branch(nx, ny, nx2, ny2, nlength, redux - 0.1, theta + turnBias, turnBias, graphics);
+                branch(nx, ny, nx2, ny2, nlength, redux - 0.1, theta - turnBias, turnBias, graphics);
 
                 //branch();
             }
+
+            else {
+
+            }
+
             //graphics.DrawCurve(penny, )
         }
     }
